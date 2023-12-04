dae6a03107c6826bb090b22d7103876af3c562ee
Aldrich-Fernandes <aldrich.fernandes05@gmail.com>
2023-12-04 14:19:22 +0000

    Created seperate file for Layers

---
+++ Scripts/Layer.py
@@ -0,0 +1,62 @@
+from ActivationLossAndOptimizers import ReLU, Sigmoid
+from DataHandle import DataMethod as DM
+from math import sqrt
+from random import gauss
+
+class Layer:
+    def __init__(self, NoOfInputs, NoOfNeurons, activation="Sigmoid", regularisationStrenght=0.001):
+        if activation == "Sigmoid":
+            self.activation = Sigmoid()
+        elif activation == "ReLU":
+            self.activation = ReLU()
+
+        # Xavier/Glorot weight initialization
+        if self.activation.getID() == "ReLU":
+            Numerator = 2
+        elif self.activation.getID() == "Sigmoid":
+            Numerator = 1
+
+        scale = sqrt(Numerator / (NoOfInputs+NoOfNeurons))
+        self.__weights = [[gauss(0, scale) for _ in range(NoOfNeurons)] for _ in range(NoOfInputs)]
+        self.__biases = [0.0 for x in range(NoOfNeurons)]
+
+        # Velocity for use with self.Optimizer
+        self.__weightsVelocity = [[1e-3 for _ in range(NoOfNeurons)] for _ in range(NoOfInputs)]
+        self.__biasesVelocity = [1e-3 for _ in range(NoOfNeurons)]
+
+        # L2 regularisation
+        self.regularisationStrenght = regularisationStrenght
+
+    def forward(self, inputs):
+        self.inputs = inputs.copy() # (90x11)
+
+        self.output = [[a+b for a,b in zip(sample, self.__biases)] for sample in DM.DotProduct(inputs, self.__weights)]        
+
+        self.activation.forward(self.output)
+
+    def backward(self, dvalues):
+        self.activation.backward(dvalues)
+        dvalues = self.activation.dinputs.copy()
+
+        self.dinputs = DM.DotProduct(dvalues, DM.Transpose(self.__weights))
+
+        self.dweights = DM.DotProduct(DM.Transpose(self.inputs), dvalues) 
+
+        # result = np.sum(dvalues, axis=0, keepdims=True)
+        self.dbiases = [sum(x) for x in DM.Transpose(dvalues)]
+
+        if self.regularisationStrenght != 0:
+            DweightsRegularisation = DM.Multiply(self.regularisationStrenght, self.__weights)
+            self.dweights = [[a+(2*b) for a, b in zip(self.dweights[x], DweightsRegularisation[x])] for x in range(len(self.dweights))]
+
+    def getVelocities(self):
+        return self.__weightsVelocity, self.__biasesVelocity
+    
+    def setVelocities(self, veloWeights, veloBiases):
+        self.__weightsVelocity, self.__biasesVelocity = veloWeights, veloBiases
+
+    def getWeightsAndBiases(self):
+        return self.__weights, self.__biases
+    
+    def setWeightsAndBiases(self, weights, biases):
+        self.__weights, self.__biases = weights, biases

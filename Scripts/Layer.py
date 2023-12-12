630c476305222a647157993d39ce2ae9d27e17f9
Aldrich-Fernandes <aldrich.fernandes05@gmail.com>
2023-12-12 09:53:19 +0000

    Annotated

--- Scripts/Layer.py
+++ Scripts/Layer.py
@@ -5,6 +5,8 @@ from random import gauss
 
 class Layer:
     def __init__(self, NoOfInputs, NoOfNeurons, activation="Sigmoid", regularisationStrenght=0.001):
+
+        # Initilise Activation
         if activation == "Sigmoid":
             self.activation = Sigmoid()
             Numerator = 1
@@ -13,7 +15,6 @@ class Layer:
             Numerator = 2
 
         # Xavier/Glorot weight initialization
-
         scale = sqrt(Numerator / (NoOfInputs+NoOfNeurons))
         self.__weights = [[gauss(0, scale) for _ in range(NoOfNeurons)] for _ in range(NoOfInputs)]
         self.__biases = [0.0 for x in range(NoOfNeurons)]
@@ -25,8 +26,8 @@ class Layer:
         # L2 regularisation - Adds a penalty to prevent overfitting and improve generalisation
         self.regularisationStrenght = regularisationStrenght
 
-    def forward(self, inputs):
-        self.inputs = inputs.copy() # (90x11)
+    def forward(self, inputs): # Formula = sum(weights x input) + bias
+        self.inputs = inputs.copy()
 
         self.output = [[a+b for a,b in zip(sample, self.__biases)] for sample in DM.DotProduct(inputs, self.__weights)]        
 
@@ -36,17 +37,17 @@ class Layer:
         self.activation.backward(dvalues)
         dvalues = self.activation.dinputs.copy()
 
-        self.dinputs = DM.DotProduct(dvalues, DM.Transpose(self.__weights))
+        self.dinputs = DM.DotProduct(dvalues, DM.Transpose(self.__weights))         # Layer's dvalues
 
-        self.dweights = DM.DotProduct(DM.Transpose(self.inputs), dvalues) 
+        self.dweights = DM.DotProduct(DM.Transpose(self.inputs), dvalues)           # used by optimizer to adjust weights
 
-        # result = np.sum(dvalues, axis=0, keepdims=True)
-        self.dbiases = [sum(x) for x in DM.Transpose(dvalues)]
+        self.dbiases = [sum(x) for x in DM.Transpose(dvalues)]                      # used by optimizer to adjist biases
 
-        if self.regularisationStrenght != 0:
+        if self.regularisationStrenght != 0:                                        # adds penalty
             DweightsRegularisation = DM.Multiply(self.regularisationStrenght, self.__weights)
             self.dweights = [[a+(2*b) for a, b in zip(self.dweights[x], DweightsRegularisation[x])] for x in range(len(self.dweights))]
 
+    # Getters and Setters
     def getVelocities(self):
         return self.__weightsVelocity, self.__biasesVelocity
     

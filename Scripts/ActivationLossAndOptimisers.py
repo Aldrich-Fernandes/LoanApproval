1a2d3ac9bd9df3b0f149701e3649eeaebd48945b
Aldrich-Fernandes <aldrich.fernandes05@gmail.com>
2024-01-13 11:18:35 +0000

    Annotated code

--- Scripts/ActivationLossAndOptimisers.py
+++ Scripts/ActivationLossAndOptimisers.py
@@ -3,18 +3,26 @@ from abc import ABC, abstractmethod
 
 from DataHandle import DataMethod as DM
 
-#Activations
-class Activation(ABC): # Scales the output of a layer
+'''
+Activations
+
+Introduces more flexibility to the network, allowing it to understand non-linear and more complex relationships and 
+patterns in the data/between features.
+
+'''
+class Activation(ABC):
+    # Contains formula to pass data through
     @abstractmethod
-    def forward(self, inputs): # Formula
+    def forward(self, inputs): 
         pass
 
+    # Uses the dirative of the formula to calculate how much of the error is caused by the function hence how much to adjust the parameters
     @abstractmethod
-    def backward(self, dvalues): # Diretive of the Formula
+    def backward(self, dvalues):
         pass
 
 class ReLU(Activation): # Rectified Linear Unit
-    # limits between (>= 0)
+    # limits inputs between (>= 0)
     def forward(self, inputs):
         self.inputs = inputs
         self.outputs = [[max(0, element) for element in entry]
@@ -32,60 +40,76 @@ class Sigmoid(Activation):
     # Squashes data between 0 and 1
     def forward(self, inputs): 
 
-        # Introduced different 
-        self.outputs = [self.__negative(val[0]) if val[0] < 0 else self.__positive(val[0]) for val in inputs] # avoids overflow errors with exp()
+        # Prevents overflow errors with the exp() function
+        self.outputs = [self.__negative(val[0]) if val[0] < 0 else self.__positive(val[0]) for val in inputs]
 
     def backward(self, dvalues):
         self.dinputs = [[a*b*(1-b)] for a,b in zip(dvalues, self.outputs)]
 
-# Loss - Measure how well the model performed
+'''
+Loss 
+
+Measures how well the model performed by comparing the true and predicted values
+
+The algorithm doesn't utilise the calculated loss value directly. It is use to visualise if the model is improving when 
+training and identify what is impacting the model and how does does it. 
+'''
 class BinaryCrossEntropy:
     def __init__(self, regularisationStrenght=0.0):
         self.__sampleLoss = 0.0
         self.__regularisationLoss = 0.0
+
+        # How strongly to penalise the model  
         self.__regularisationStrenght = regularisationStrenght
 
     def forward(self, predictions, TrueVals):
-        # Remove any 0s or 1s to avoid arithmethic errors
         predictions = clipEdges(predictions)
 
         # Formula used: -(true * log(Predicted) + (1 - true) * log(1 - Predicted))
         sampleLoss = [-((tVal * log(pVal)) + ((1 - tVal) * log(1 - pVal))) for tVal, pVal in zip(TrueVals, predictions)]
 
-        self.__sampleLoss = sum(sampleLoss) / len(sampleLoss) # Average of all samples
+        self.__sampleLoss = sum(sampleLoss) / len(sampleLoss)       # Average of all samples
 
-        self.__regularisationLoss = 0 # Reset for that epoch
+        self.__regularisationLoss = 0                               # Resets the loss for that epoch
 
     # Dirative of above Formula
     def backward(self, predicted, TrueVals): 
         predicted = clipEdges(predicted)
         
-        # Formula == (PredictVal - Tval) / ((1-PredictVal) * PredictVal)
+        # Dirative of formula above used: (PredictVal - Tval) / ((1-PredictVal) * PredictVal)
         self.dinputs = [(PredictVal - Tval) / ((1-PredictVal) * PredictVal) for Tval, PredictVal in zip(TrueVals, predicted)]
 
+    # For adjust the hyperparameter when training a new model
     def updateRegStr(self, regStr):
         self.__regularisationStrenght = regStr
 
     def calcRegularisationLoss(self, layerWeights):
-        
         if self.__regularisationStrenght != 0:
             self.__regularisationLoss += 0.5 * self.__regularisationStrenght * sum([sum(x) for x in DM.Multiply(layerWeights, layerWeights)])
 
     def getLoss(self):
         return self.__sampleLoss + self.__regularisationLoss
 
+'''
+Optimser
 
+Improve the accuracy of the model .
+
+Does this by adjusting the weights and biases of layers by adding/subtrating an small amount to the weights depending 
+on their impact on the model and it's output which is calculated in the backpass (utilises the dvalues).
+'''
 class OptimiserSGD:
     def __init__(self, InitialLearningRate=1e-4, decay=5e-5, momentum=0.95, mode="Linear"):
         self.__InitialLearningRate = InitialLearningRate          # Starting Learning rate
         self.__minimumLearningRate = InitialLearningRate * 0.001  # Lower bound Leanring rate
         self.__decay = decay                                      # Rate at which Learning rate decreases
         self.__momentum = momentum                                # Makes Accuracy and Loss change in a consistant way in one direction
-        self.activeLearningRate = InitialLearningRate             # Working learning rate
+        self.activeLearningRate = InitialLearningRate             # How much to adjust the layer weights and biases. 
 
         self.__mode = mode
 
-    def adjustLearningRate(self, iter): # gradually decreases the learning rate to avoid overshooting the optimal parameters
+    # gradually decreases the learning rate to avoid overshooting the optimal parameters
+    def adjustLearningRate(self, iter): 
         if self.__decay != 0:
             if self.__mode == "Linear":
                 self.activeLearningRate = max(self.__InitialLearningRate / (1 + self.__decay * iter), self.__minimumLearningRate)
@@ -94,32 +118,38 @@ class OptimiserSGD:
 
     # Function to update the parameters of a neural network layer using SGD with momentum
     def UpdateParameters(self, layer): 
-        AdjustedDWeight = DM.Multiply(self.activeLearningRate, layer.dweights)
-        AdjustedDBiases = DM.Multiply(self.activeLearningRate, layer.dbiases)
+
+        # Amount to add to the weights and biases
+        weightUpdate = DM.Multiply(self.activeLearningRate, layer.dweights)
+        biasesUpdate = DM.Multiply(self.activeLearningRate, layer.dbiases)
 
         weights, biases = layer.getWeightsAndBiases()
 
         if self.__momentum != 0:
-            weightsVelocity, biasesVelocity = layer.getVelocities()
 
-            # Velocity = momentum * Velocity - activeLearningRate * dweights
-            weightsVelocity = [[a - b for a, b in zip(velocityRow, dweightsRow)]
-                               for velocityRow, dweightsRow in zip(DM.Multiply(self.__momentum, weightsVelocity),
-                                                                  AdjustedDWeight)]
+            # Amount to add to the weights and biases to reduce fluctuations in accuracy and loss
+            weightVelocityUpdate, biasesVelocityUpdate = layer.getVelocities()
 
-            # Velocity = momentum * Velocity - activeLearningRate * dbiases
-            biasesVelocity = [a - b for a, b in zip(DM.Multiply(self.__momentum, biasesVelocity), AdjustedDBiases)]
+            # New weight velocity = momentum * Velocity - activeLearningRate * dweights
+            weightVelocityUpdate = [[a - b for a, b in zip(velocityRow, dweightsRow)]
+                               for velocityRow, dweightsRow in zip(DM.Multiply(self.__momentum, weightVelocityUpdate),
+                                                                  weightUpdate)]
 
-            layer.setVelocities(weightsVelocity, biasesVelocity)
+            # New bias velocity = momentum * Velocity - activeLearningRate * dbiases
+            biasesVelocityUpdate = [a - b for a, b in zip(DM.Multiply(self.__momentum, biasesVelocityUpdate), biasesUpdate)]
 
-            weights = [[a + b for a, b in zip(weights[x], weightsVelocity[x])] for x in range(len(weights))]
-            biases = [a + b for a, b in zip(biases, biasesVelocity)]
+            layer.setVelocities(weightVelocityUpdate, biasesVelocityUpdate)
+
+            # Final Updates
+            weights = [[a + b for a, b in zip(weights[x], weightVelocityUpdate[x])] for x in range(len(weights))]
+            biases = [a + b for a, b in zip(biases, biasesVelocityUpdate)]
         else:
-            weights = [[a - b for a, b in zip(weights[x], AdjustedDWeight[x])] for x in range(len(weights))]
-            biases = [a - b for a, b in zip(biases, AdjustedDBiases)]
+            weights = [[a - b for a, b in zip(weights[x], weightUpdate[x])] for x in range(len(weights))]
+            biases = [a - b for a, b in zip(biases, biasesUpdate)]
 
         layer.setWeightsAndBiases(weights, biases)
 
+# Replaces any 0s or 1s to avoid arithmethic errors
 def clipEdges(list, scale=1e-7):
     for index, val in enumerate(list):
         if val < scale:

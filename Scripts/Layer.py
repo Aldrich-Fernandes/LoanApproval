13fe09ddfbfb9596e63dbc878a0667e36ac1e7f9
Aldrich-Fernandes <aldrich.fernandes05@gmail.com>
2024-01-16 21:30:48 +0000

    Finalised

--- Scripts/Layer.py
+++ Scripts/Layer.py
@@ -5,8 +5,8 @@ from random import gauss
 
 class Layer:
     def __init__(self, NoOfInputs, NoOfNeurons, activation="Sigmoid", regularisationStrength=0.001):
-        self._NoOfInputs = NoOfInputs
-        self._NoOfNeurons = NoOfNeurons
+        self._NoOfInputs = NoOfInputs           # Number of neurons/inputs in the previous layer
+        self._NoOfNeurons = NoOfNeurons         # Number of neurons/inputs in this layer
 
         # L2 regularisation - Adds a penalty to prevent overfitting and improve generalisation
         self.__regStr = regularisationStrength
@@ -21,9 +21,10 @@ class Layer:
 
         self.initialiseNewLayer()
 
+    # Creates new random weights and biases for the layer.
     def initialiseNewLayer(self): 
         # Xavier/Glorot weight initialization
-        # Creates a dense layer inistalisec with a small value
+        # Creates a dense layer initialised with a small value
         scale = sqrt(self.__Numerator / (self._NoOfInputs+self._NoOfNeurons))
         self.__weights = [[gauss(0, scale) for _ in range(self._NoOfNeurons)] 
                           for _ in range(self._NoOfInputs)] 
@@ -33,7 +34,7 @@ class Layer:
         self.__weightsVelocity = [[1e-3 for _ in range(self._NoOfNeurons)] for _ in range(self._NoOfInputs)]
         self.__biasesVelocity = [1e-3 for _ in range(self._NoOfNeurons)]
 
-    # Formula = sum(weights x input) + bias
+    # Formula = DotProduct(input, weights) + bias
     def forward(self, inputs):
         self.inputs = inputs.copy()
 
@@ -42,26 +43,27 @@ class Layer:
 
         self.activation.forward(self.output)
 
+    # Calculates dvalues, dinputs, dweights, dbiases which are gradients that 
+    # helps shows how much these attributes impacted the prediction
     def backward(self, dvalues):
-        # dvalues, dinputs, dweights, dbiases: are gradients show how much it impacted the results
 
         self.activation.backward(dvalues)
-        dvalues = self.activation.dinputs.copy()
+        dvalues = self.activation.dinputs.copy() # gradients from the activation function
 
-        # Layer's dvalues to be passes to the next layer
+        # Layer's dvalues (gradients) to be passes to the next layer
         self.dinputs = DM.DotProduct(dvalues, DM.Transpose(self.__weights)) 
 
-        # used by optimizer to adjust weights and biases
+        # Used by optimizer to adjust weights and biases
         self.dweights = DM.DotProduct(DM.Transpose(self.inputs), dvalues)
         self.dbiases = [sum(x) for x in DM.Transpose(dvalues)]
 
         # L2 regularisation prevents a single weight from getting to large.
         if self.__regStr != 0:
-            DweightsReg = DM.Multiply(self.__regStr, self.__weights)
-            self.dweights = [[a+(2*b) for a, b in zip(self.dweights[x], DweightsReg[x])] 
+            DweightsSqr = DM.Multiply(self.__regStr, self.__weights)
+            self.dweights = [[a+(2*b) for a, b in zip(self.dweights[x], DweightsSqr[x])] 
                              for x in range(len(self.dweights))]
 
-    # Getters and Setters
+    # Getters and setters used by the optimiser to retrive and adjust the weights and biases
     def getVelocities(self):
         return self.__weightsVelocity, self.__biasesVelocity
     
@@ -71,5 +73,6 @@ class Layer:
     def getWeightsAndBiases(self):
         return self.__weights, self.__biases
     
+    # Also used to load a pretrained model
     def setWeightsAndBiases(self, weights, biases):
         self.__weights, self.__biases = weights, biases

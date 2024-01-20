2562298b008d4140dfa1b22e60770e58c9b2692b
Aldrich-Fernandes <aldrich.fernandes05@gmail.com>
2024-01-20 19:21:42 +0000

    Annotated

--- Scripts/pilot.py
+++ Scripts/pilot.py
@@ -1,29 +1,36 @@
+# Models used
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+
+# Unitilites
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
+# From my programs
 from DataHandle import PreProcess
 
-PreProcessor = PreProcess()
-LogRegAcc = []
-RandForestAcc = []
-
-def LogReg(X_train, X_test, y_train, y_test):
+# random forest classification - utilises tree traversal
+def RandForest(X_train, X_test, y_train, y_test):
     acc = []
-    for _ in range(10):
-        model = LogisticRegression()
+    for _ in range(10):     # Generates 10 different models to get average acurracy.
+        # Initialising a model
+        model = RandomForestClassifier(n_estimators=100, random_state=42)
+
+        # Training the model
         model.fit(X_train, y_train)
-        y_pred = model.predict(X_test)
-        accuracy = accuracy_score(y_test, y_pred)
+
+        # Testing performace
+        predictions = model.predict(X_test)
+        accuracy = accuracy_score(y_test, predictions)
         acc.append(accuracy)
 
     return round(sum(acc)/len(acc), 4)
 
-def RandForest(X_train, X_test, y_train, y_test):
+# Logistic regression model - ultilies neural networks
+def LogReg(X_train, X_test, y_train, y_test):   # Same as RanForest()
     acc = []
     for _ in range(10):
-        model = RandomForestClassifier(n_estimators=100, random_state=42)
+        model = LogisticRegression()
         model.fit(X_train, y_train)
         predictions = model.predict(X_test)
         accuracy = accuracy_score(y_test, predictions)
@@ -31,8 +38,12 @@ def RandForest(X_train, X_test, y_train, y_test):
 
     return round(sum(acc)/len(acc), 4)
 
-def testModels():
-    for x in range(10):
+def testModels():        
+    PreProcessor = PreProcess()
+    LogRegAcc = []
+    RandForestAcc = []
+
+    for x in range(10): # Trains models with 10 different datasets.
         print(f"Training dataset {x+1}")
         PreProcessor.newDataset()
         TrainX, TrainY, _, _ = PreProcessor.getData(split=0)
@@ -41,6 +52,7 @@ def testModels():
         LogRegAcc.append(LogReg(X_train, X_test, y_train, y_test))
         RandForestAcc.append(RandForest(X_train, X_test, y_train, y_test))
 
+    # Average accuracy after 100 tests each. 
     print(f"Logistic Regression     | Accuracy: {round(sum(LogRegAcc)/len(LogRegAcc), 4)}")
     print(f"Random Forest           | Accuracy: {round(sum(RandForestAcc)/len(RandForestAcc), 4)}")
 

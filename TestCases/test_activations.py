bf75a53a04cfc4f47783fed6ceee152c2af08f9b
Aldrich-Fernandes <aldrich.fernandes05@gmail.com>
2024-01-30 09:26:22 +0000

    Created test

--- TestCases/test_activations.py
+++ TestCases/test_activations.py
@@ -4,37 +4,37 @@ import unittest
 
 class TestActivations(unittest.TestCase):
     
-    def reluForward(self):
+    def test_reluForward(self):
         relu = ReLU()
         inputs = [[-1, 0, 1], [2, -2, 3]]
         relu.forward(inputs)
         expected = [[0, 0, 1], [2, 0, 3]]
         self.assertEqual(relu.outputs, expected)
 
-    def reluBackward(self):
+    def test_reluBackward(self):
         relu = ReLU()
         inputs = [[-1, 0, 1], [2, -2, 3]]
         relu.forward(inputs)
-        dvalues = [[1, 2, 3], [4, 5, 6]]
+        dvalues = None
         relu.backward(dvalues)
-        expected = [[0, 0, 3], [4, 0, 6]]
+        expected = [[0, 0, 1], [1, 0, 1]]
         self.assertEqual(relu.dinputs, expected)
 
-    def sigmoidForward(self):
+    def test_sigmoidForward(self):
         sigmoid = Sigmoid()
         inputs = [[-1], [0], [1]]
         sigmoid.forward(inputs)
         expected = [0.2689414213699951, 0.5, 0.7310585786300049]
-        self.assertEqual(sigmoid.outputs, expected)
+        self.assertAlmostEqual(sigmoid.outputs, expected)
 
-    def sigmoidBackward(self):
+    def test_sigmoidBackward(self):
         sigmoid = Sigmoid()
         inputs = [[-1], [0], [1]]
         sigmoid.forward(inputs)
-        dvalues = [[1], [2], [3]]
+        dvalues = [-3.7182818284590455, -2.0, -1.3678794411714423]
         sigmoid.backward(dvalues)
-        expected = [[0.19661193324148185], [0.0], [0.19661193324148185]]
-        self.assertEqual(sigmoid.dinputs, expected)
+        expected = [[-0.7310585786300049], [-0.5], [-0.2689414213699951]]
+        self.assertAlmostEqual(sigmoid.dinputs, expected)
 
 if __name__ == '__main__':
     unittest.main()

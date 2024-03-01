a3c183b7053d501546d1b7b3442a05b64b865b99
Aldrich-Fernandes <aldrich.fernandes05@gmail.com>
2024-03-01 21:19:19 +0000

    Added tests

--- TestCases/test_activations.py
+++ TestCases/test_activations.py
@@ -1,41 +1,42 @@
-from Scripts.Activations import ReLU, Sigmoid
-
 import unittest
 
+from Scripts.NeuralNetwork import ReLU
+from Scripts.NeuralNetwork import Sigmoid
+
 class TestRectifiedLinearUnit(unittest.TestCase):
+    def setUp(self):
+        self.relu = ReLU()
     
     def test_reluForward(self):
-        relu = ReLU()
+        self.relu = ReLU()
         inputs = [[-1, 0, 1], [2, -2, 3]]
-        relu.forward(inputs)
+        self.relu.forward(inputs)
         expected = [[0, 0, 1], [2, 0, 3]]
-        self.assertEqual(relu.outputs, expected)
+        self.assertEqual(self.relu.outputs, expected)
 
     def test_reluBackward(self):
-        relu = ReLU()
+        self.relu = ReLU()
         inputs = [[-1, 0, 1], [2, -2, 3]]
-        relu.forward(inputs)
+        self.relu.forward(inputs)
         dvalues = None
-        relu.backward(dvalues)
+        self.relu.backward(dvalues)
         expected = [[0, 0, 1], [1, 0, 1]]
-        self.assertEqual(relu.dinputs, expected)
+        self.assertEqual(self.relu.dinputs, expected)
 
 class TestSigmoid(unittest.TestCase):
+    def setUp(self):
+        self.sigmoid = Sigmoid()
+
     def test_sigmoidForward(self):
-        sigmoid = Sigmoid()
         inputs = [[-1], [0], [1]]
-        sigmoid.forward(inputs)
+        self.sigmoid.forward(inputs)
         expected = [0.2689414213699951, 0.5, 0.7310585786300049]
-        self.assertAlmostEqual(sigmoid.outputs, expected)
+        self.assertAlmostEqual(self.sigmoid.outputs, expected)
 
     def test_sigmoidBackward(self):
-        sigmoid = Sigmoid()
         inputs = [[-1], [0], [1]]
-        sigmoid.forward(inputs)
+        self.sigmoid.forward(inputs)
         dvalues = [-3.7182818284590455, -2.0, -1.3678794411714423]
-        sigmoid.backward(dvalues)
+        self.sigmoid.backward(dvalues)
         expected = [[-0.7310585786300049], [-0.5], [-0.2689414213699951]]
-        self.assertAlmostEqual(sigmoid.dinputs, expected)
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertAlmostEqual(self.sigmoid.dinputs, expected)
--- TestCases/test_loss.py
+++ TestCases/test_loss.py
@@ -1,33 +1,32 @@
-from Scripts.LossAndOptimiser import BinaryCrossEntropy
+from Scripts.NeuralNetwork import BinaryCrossEntropy
 
 import unittest
 
 class TestBinaryCrossEntropy(unittest.TestCase):
+    def setUp(self):
+        self.LossFunction = BinaryCrossEntropy(regStr=0.001)
 
     def test_forward(self):
-        LossFunction = BinaryCrossEntropy(regStr=0.001)
         predictions = [0.2, 0.7, 0.9]
         TrueValues = [0, 1, 1]
-        LossFunction.forward(predictions, TrueValues)
+        self.LossFunction.forward(predictions, TrueValues)
         ExpectedLoss = -((0 * 0.2 + (1 - 0) * 0.7 + (1 - 0) * 0.9) / 3)
-        self.assertAlmostEqual(LossFunction.getLoss(), ExpectedLoss, places=7)
+        self.assertAlmostEqual(self.LossFunction.getLoss(), ExpectedLoss, places=7)
 
     def test_backward(self):
-        LossFunction = BinaryCrossEntropy(regStr=0.001)
         predictions = [0.2, 0.7, 0.9]
         TrueValues = [0, 1, 1]
-        LossFunction.backward(predictions, TrueValues)
+        self.LossFunction.backward(predictions, TrueValues)
         ExpectedGradients = [(0.2 - 0) / ((1 - 0.2) * 0.2),
                               (0.7 - 1) / ((1 - 0.7) * 0.7),
                               (0.9 - 1) / ((1 - 0.9) * 0.9)]
-        self.assertListEqual(LossFunction.dinputs, ExpectedGradients)
+        self.assertListEqual(self.LossFunction.dinputs, ExpectedGradients)
 
     def test_calcRegularisationLoss(self):
-        LossFunction = BinaryCrossEntropy(regStr=0.001)
         layer_weights = [[0.1, 0.2], [0.3, 0.4]]
-        LossFunction.calcRegularisationLoss(layer_weights)
+        self.LossFunction.calcRegularisationLoss(layer_weights)
         ExpectedLoss = 0.5 * 0.001 * (0.1**2 + 0.2**2 + 0.3**2 + 0.4**2)
-        self.assertAlmostEqual(LossFunction.getLoss(), ExpectedLoss, places=7)
+        self.assertAlmostEqual(self.LossFunction.getLoss(), ExpectedLoss, places=7)
 
 if __name__ == '__main__':
     unittest.main()

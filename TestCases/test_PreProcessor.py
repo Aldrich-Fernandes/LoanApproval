7ae5185ae537b9cdf65dbca096941a9373762d2b
Aldrich-Fernandes <aldrich.fernandes05@gmail.com>
2024-03-01 22:55:35 +0000

    added frames for data handling tests

---
+++ TestCases/test_PreProcessor.py
@@ -0,0 +1,20 @@
+from Scripts.DataHandle import PreProcess
+
+import unittest
+
+class TestPreprocessor(unittest.TestCase):
+    def setUp(self):
+        self.processor = PreProcess()
+
+    def test_newDataset(self):
+        pass # creates a new dataset and tests the following:
+                    # if the data is a 2d array of correct dimentions of only floats
+
+    def test_DataIsStandardised(self):
+        pass
+
+    def test_GetDataSplit(self):
+        pass
+
+    def test_encode(self):
+        pass

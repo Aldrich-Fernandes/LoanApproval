5b5980f282b813468e3ae9d26b268652d29398e6
Aldrich-Fernandes <aldrich.fernandes05@gmail.com>
2024-03-02 12:22:18 +0000

    Updated Scope

--- Scripts/NeuralNetwork/Layer/Activations/ReLU.py
+++ Scripts/NeuralNetwork/Layer/Activations/ReLU.py
@@ -3,10 +3,10 @@ from .ActivationABC import Activation
 class ReLU(Activation): # Rectified Linear Unit
     # limits inputs between (>= 0)
     def forward(self, inputs):
-        self.inputs = inputs
+        self.__inputs = inputs
         self.outputs = [[max(0, element) for element in entry]
                         for entry in inputs]
 
     # if input value was > 0 then gradient = 1
     def backward(self, _):
-        self.dinputs = [[1 if element > 0 else 0 for element in sample] for sample in self.inputs] 
+        self.dinputs = [[1 if element > 0 else 0 for element in sample] for sample in self.__inputs] 

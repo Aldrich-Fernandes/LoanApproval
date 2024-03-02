b8bcfe66bfc3c98e44878e5a9cff16a116bb9699
Aldrich-Fernandes <aldrich.fernandes05@gmail.com>
2024-03-02 11:57:56 +0000

    .

--- Scripts/Activations/__init__.py
+++
@@ -1,9 +0,0 @@
-from .ActivationABC import Activation
-from .ReLU import ReLU
-from .Sigmoid import Sigmoid
-
-__all__ = [
-    "Activation",
-    "ReLU",
-    "Sigmoid"
-]
--- Scripts/LossAndOptimiser/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Loss import BinaryCrossEntropy
-from .Optimiser import OptimiserSGD
-
-__all__ = [
-    "BinaryCrossEntropy",
-    "OptimiserSGD"
-]
